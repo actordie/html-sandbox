91265960cab8db8f59bec24a1ea4d9dbf904c19a
S JACKSON <jackson.sabastian@gmail.com>
2017-04-12 08:22:23 -0600

    good push place

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<!-- Custom CSS - only html code looks like this keeping organized is important in html
+		 Javascript is placed in the head
+		 -->
+		<link rel="stylesheet" href="css/STYLEMATTERS.css" type="text/css" />
+
 		<title>HTML SANBOX</title>
 	</head>
 	<body>
